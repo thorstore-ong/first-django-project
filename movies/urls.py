5eb9b39f3542804694041eeceb06c94e675ef885
Thoriso Mokhethi <thoriso.mokhethi@mpilo.africa>
2024-04-09 12:03:14 +0200

    created movies page and added html

--- movies/urls.py
+++ movies/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from . import views 
+import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', views.movies)
+    path('', views.home, name='home'),
+    path('movies/', views.movies, name='movies')
 ]
--- views.py
+++ views.py
@@ -1,8 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
 
-
-
-from http.client import HTTPResponse
-
+data = {
+   'movies': [
+       {
+         'id': 5,
+         'title': 'Jaws',
+         'year': 1669,
+       },
+       {
+         'id': 6,
+         'title': 'Sharknado',
+         'year': 1600 ,
+       },
+       {
+         'id': 5,
+         'title': 'The Meg',
+         'year': 2000,
+       }
+   ]
+}
 
 def movies(request):
-    return HTTPResponse("Hello there!")
+    return render(request, 'movies/movies.html', data)
+
+def home(request):
+    return HttpResponse("Home page")
